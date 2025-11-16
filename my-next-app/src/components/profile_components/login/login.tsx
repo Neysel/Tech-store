30b4245cd19cc020e3a48bc4ea1ef478b6e1d5d7
Neysel <neyseltheblack@gmail.com>
2025-11-17 02:37:49 +0400

    add raw login logout and register files

---
+++ my-next-app/src/components/profile_components/login/login.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+const Login = () => {
+    return ( <>
+    </> );
+}
+ 
+export default Login;
---
+++ my-next-app/src/components/profile_components/register/register.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+const Register = () => {
+    return ( <>
+    </> );
+}
+ 
+export default Register;
